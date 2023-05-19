ca8d621943ef0dc13f1bd18baa2da50a22f2535c
Dikhi Martin <dikhi.martin@gmail.com>
2023-05-20 00:14:56 +0700

    draft:Bar Chart

--- project/resources/views/backend/report/transaction-report/index.blade.php
+++ project/resources/views/backend/report/transaction-report/index.blade.php
@@ -62,13 +62,26 @@
 		@endslot
 	@endcomponent	
 
-	<!--begin::Content-->
-    <div id="kt_app_content" class="app-content flex-column-fluid">
-		<!--begin::Content container-->
+	<!--begin::Card Grafik -->
+	<div id="kt_app_content" class="app-content flex-column-fluid">
         <div id="kt_app_content_container" class="app-container container-fluid">
-			<!--begin::Card-->
 			<div class="card">
+				<div class="card-header">
+					<h3 class="card-title align-items-start flex-column">
+						<span class="card-label fw-bold text-gray-800">Laporan Grafik</span>
+					</h3>
+				</div>
+				<div class="card-body">
+					<div id="bar-chart-transaction"></div>
+				</div>
+			</div>
+		</div>
+	</div>
 
+	<!--begin::Card Table -->
+    <div id="kt_app_content" class="app-content flex-column-fluid">
+        <div id="kt_app_content_container" class="app-container container-fluid">
+			<div class="card">
 				<!--begin::Header Datatable Component -->
 				@component('backend.components.datatable-header')
 				@endcomponent
@@ -108,6 +121,9 @@
 <!--begin::Vendors Javascript(used for this page only)-->
 @push('private_js')
 	<script src="{{ URL::asset('assets/plugins/custom/datatables/datatables.bundle.js') }}"></script>
+	<script src="{{ URL::asset('assets/plugins/custom/highcharts/highcharts.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/custom/highcharts/exporting.js') }}"></script>
+    <script src="{{ URL::asset('assets/plugins/custom/highcharts/series-label.js') }}"></script>
 	<script>
 		"use strict";
 
@@ -407,6 +423,7 @@
 				// Filter datatable on submit
 				filterButton.addEventListener('click', function () {
 					dt.ajax.url(URL_API + generateFilter()).load();
+					barChartReport();
 				});
 			}
 
@@ -428,6 +445,7 @@
 					});
 
 					dt.ajax.url(URL_API).load();
+					barChartReport();
 				});
 			}
 
@@ -447,9 +465,83 @@
 			}
 		}();
 
+		// Bar Chart
+		var barChartReport = () => {
+			Highcharts.chart('bar-chart-transaction', {
+				chart: {
+					type: 'column'
+				},
+				title: {
+					text: 'Monthly Average Rainfall'
+				},
+				subtitle: {
+					text: 'Source: WorldClimate.com'
+				},
+				xAxis: {
+					categories: [
+						'Jan',
+						'Feb',
+						'Mar',
+						'Apr',
+						'May',
+						'Jun',
+						'Jul',
+						'Aug',
+						'Sep',
+						'Oct',
+						'Nov',
+						'Dec'
+					],
+					crosshair: true
+				},
+				yAxis: {
+					min: 0,
+					title: {
+						text: 'Rainfall (mm)'
+					}
+				},
+				tooltip: {
+					headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+					pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+						'<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+					footerFormat: '</table>',
+					shared: true,
+					useHTML: true
+				},
+				plotOptions: {
+					column: {
+						pointPadding: 0.2,
+						borderWidth: 0
+					}
+				},
+				series: [{
+					name: 'Tokyo',
+					data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4,
+						194.1, 95.6, 54.4]
+
+				}, {
+					name: 'New York',
+					data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5,
+						106.6, 92.3]
+
+				}, {
+					name: 'London',
+					data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3,
+						51.2]
+
+				}, {
+					name: 'Berlin',
+					data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8,
+						51.1]
+
+				}]
+			});
+		}
+
 		// On document ready
 		KTUtil.onDOMContentLoaded(function () {
 			KTDatatablesServerSide.init();
+			barChartReport();
 		});
 	</script>
 @endpush
